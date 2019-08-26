3eef3ded5d698d71ed13008da303e6f4ef071dce
Wilson Sheldon <wilson@namedia.net>
2019-08-26 14:40:30 -0400

    Initial Commit

---
+++ app/_config.php
@@ -0,0 +1,11 @@
+<?php
+
+global $databaseConfig;
+$databaseConfig = [
+    "type" => "MySQLDatabase",
+    "server" => "wilsontest.cyzgvcttarhk.us-east-1.rds.amazonaws.com",
+    "username" => 'admin',
+    "password" => 'admin123',
+    "database" => 'breftest',
+
+];
