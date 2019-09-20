24a1b0b3a3d091bb34785db3798110928fee2343
Wilson Sheldon <wilson@namedia.net>
2019-09-20 09:27:56 -0400

    Added runtime to buildspec

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ if [ "$CODEBUILD_GIT_BRANCH" == "" ] ; then
   CODEBUILD_GIT_BRANCH=`git branch -a --contains HEAD | sed -n 2p | awk '{ printf $1 }'`
   CODEBUILD_GIT_BRANCH=${CODEBUILD_GIT_BRANCH#remotes/origin/}
 fi
+composer install
 composer vendor-expose copy
 aws s3 sync public/_resources s3://wilsonsheldon.name/_resources
 serverless deploy --stage "$CODEBUILD_GIT_BRANCH" --verbose | tee deploy.out
