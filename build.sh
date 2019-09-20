7c0eee920e8cb513af1b28902b8e3e410bb7047c
Wilson Sheldon <wilson@namedia.net>
2019-09-20 08:29:01 -0400

    Latest work

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ if [ "$CODEBUILD_GIT_BRANCH" == "" ] ; then
   CODEBUILD_GIT_BRANCH=`git branch -a --contains HEAD | sed -n 2p | awk '{ printf $1 }'`
   CODEBUILD_GIT_BRANCH=${CODEBUILD_GIT_BRANCH#remotes/origin/}
 fi
-serverless deploy --stage "$CODEBUILD_GIT_BRANCH" --region us-east-1 --verbose | tee deploy.out
+composer vendor-expose copy
+aws s3 sync public/_resources s3://wilsonsheldon.name/_resources
+serverless deploy --stage "$CODEBUILD_GIT_BRANCH" --verbose | tee deploy.out
